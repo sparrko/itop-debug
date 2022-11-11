55eb77ad832cb083dbe0152a7c40c5ee82c9cf8c
sparrko <roninblade11@yandex.ru>
2022-11-11 17:32:35 +0400

    Тестовый класс Монитор

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 <h2>gransoft-contract-addition</h2>
 <p>Тестовое учебное расширение которое добавляет в сущность договор поле ИНН контрагента, и еще валюту рубли.</p>
+
+<h2>gransoft-monitor</h2>
+<p>Тестовое учебное расширение которое добавляет сущность Монитор.</p>
+<p>Списано с <a href="https://www.itophub.io/wiki/page?id=latest:customization:add-class-sample">itophub.io</a>.</p>
---
+++ extensions/gransoft-monitor/ru.dict.gransoft-monitor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	// Dictionary entries go here
+	'Class:Monitor' => 'Монитор',
+	'Class:Monitor+' => 'Компьютерный дисплей',
+	'Class:Monitor/Attribute:technology' => 'Технология',
+	'Class:Monitor/Attribute:technology+' => 'Технология используемая для отображения',
+	'Class:Monitor/Attribute:technology/Value:crt' => 'CRT',
+	'Class:Monitor/Attribute:technology/Value:lcd' => 'LCD',
+));
+?>
